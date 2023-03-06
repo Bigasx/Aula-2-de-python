39bebe98f9ecb4d6d0f9bb16a4e04ff2e0a0db19
Bigasx <126120062+Bigasx@users.noreply.github.com>
2023-03-06 14:07:09 -0300

    Update leonardo.py

---
+++ leonardo.py
@@ -0,0 +1,8 @@
+Nome = "Leonardo Torres"
+Idade = int(input('Digite sua idade: '))
+Peso = 75
+if Idade >= 18:
+    print("Você é maioridade!")
+else:
+    print("Você não é maioridade")
+print(Nome, Idade, Peso)
